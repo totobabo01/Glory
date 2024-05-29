34e80b2e88a304db2987657ec47cc8601f5138a3
JANG HYEON SEO <totobabo01@gmail.com>
2024-05-30 00:00:25 +0900

    20240529 과제

--- Glory.cpp
+++ Glory.cpp
@@ -1,126 +1,154 @@
-﻿#include <iostream>
-#include <list>
-#include <memory>
-#include <mutex>
-#include <thread>
-#include <vector>
-#include <atomic>
-#include <chrono>
-#include <map>
+﻿//2-2.alarm clock
+//#include <iostream>
+//#include <list>
+//#include <memory>
+//#include <mutex>
+//#include <thread>
+//#include <vector>
+//#include <atomic>
+//#include <chrono>
+//#include <condition_variable>
+//#include <map>
+//#include <algorithm>
+//#include <fstream>
+//#include <sstream>
+//#include <queue>
+//
+//using namespace std;
+//
+//class Process {
+//public:
+//    int id;
+//    bool isForeground;
+//    string command;
+//    bool promoted = false;
+//    int remainingTime = 0; // for wait queue
+//
+//    Process(int id, bool isForeground, const string& command)
+//        : id(id), isForeground(isForeground), command(command), promoted(false) {}
+//
+//    string toString() const {
+//        stringstream ss;
+//        ss << "[" << id << (isForeground ? "F" : "B");
+//        if (promoted) ss << "*";
+//        ss << "]";
+//        return ss.str();
+//    }
+//};
+//
+//class DynamicQueue {
+//private:
+//    mutex mtx;
+//    condition_variable cv;
+//    list<shared_ptr<Process>> fgProcesses;
+//    list<shared_ptr<Process>> bgProcesses;
+//    map<int, shared_ptr<Process>> waitQueue;
+//    atomic<int> processCount{ 0 };
+//    atomic<int> bgCount{ 0 };
+//
+//public:
+//    void enqueue(shared_ptr<Process> process) {
+//        lock_guard<mutex> lock(mtx);
+//        if (process->isForeground) {
+//            fgProcesses.push_back(process);
+//        }
+//        else {
+//            bgProcesses.push_back(process);
+//            bgCount++;
+//        }
+//        processCount++;
+//    }
+//
+//    void simulateSleep(int pid, int seconds) {
+//        lock_guard<mutex> lock(mtx);
+//        auto proc = find_if(fgProcesses.begin(), fgProcesses.end(), [&pid](const auto& p) { return p->id == pid; });
+//        if (proc != fgProcesses.end()) {
+//            waitQueue[pid] = *proc;
+//            fgProcesses.erase(proc);
+//        }
+//        else {
+//            proc = find_if(bgProcesses.begin(), bgProcesses.end(), [&pid](const auto& p) { return p->id == pid; });
+//            if (proc != bgProcesses.end()) {
+//                waitQueue[pid] = *proc;
+//                bgProcesses.erase(proc);
+//            }
+//        }
+//        waitQueue[pid]->remainingTime = seconds;
+//    }
+//
+//    void wakeUpProcesses() {
+//        lock_guard<mutex> lock(mtx);
+//        for (auto it = waitQueue.begin(); it != waitQueue.end();) {
+//            if (--it->second->remainingTime <= 0) {
+//                enqueue(it->second);
+//                it = waitQueue.erase(it);
+//            }
+//            else {
+//                ++it;
+//            }
+//        }
+//    }
+//
+//    void printQueue() {
+//        lock_guard<mutex> lock(mtx);
+//        cout << "Running: [" << bgCount.load() << "B]\n";
+//        cout << "---------------------------\n";
+//        cout << "DQ: ";
+//        cout << "(bottom) ";
+//        for (const auto& proc : bgProcesses) {
+//            cout << proc->toString() << " ";
+//        }
+//        cout << "\nP => ";
+//        for (const auto& proc : fgProcesses) {
+//            cout << proc->toString() << " ";
+//        }
+//        cout << "(top)\n";
+//        cout << "---------------------------\n";
+//        cout << "WQ: ";
+//        for (const auto& wp : waitQueue) {
+//            cout << wp.second->toString() << " - Remaining Time: " << wp.second->remainingTime << "s; ";
+//        }
+//        cout << "\n...\n";
+//    }
+//};
+//
+//void shellProcess(DynamicQueue& dq, int interval) {
+//    vector<string> commands = { "alarm clock", "todo list", "email check" };
+//    int processId = 2; // Starting from 2, since 0 and 1 are taken by shell and monitor
+//    for (const string& line : commands) {
+//        shared_ptr<Process> proc = make_shared<Process>(processId++, true, line);
+//        dq.enqueue(proc);
+//        this_thread::sleep_for(chrono::seconds(interval));
+//        dq.printQueue();
+//    }
+//}
+//
+//void monitorProcess(DynamicQueue& dq, int interval) {
+//    while (true) {
+//        this_thread::sleep_for(chrono::seconds(interval));
+//        dq.wakeUpProcesses();
+//        dq.printQueue();
+//    }
+//}
+//
+//int main() {
+//    DynamicQueue dq;
+//    shared_ptr<Process> shellProc = make_shared<Process>(0, true, "shell");
+//    shared_ptr<Process> monitorProc = make_shared<Process>(1, false, "monitor");
+//
+//    dq.enqueue(shellProc);
+//    dq.enqueue(monitorProc);
+//
+//    thread shellThread(shellProcess, ref(dq), 5); // Simulate shell processing
+//    thread monitorThread(monitorProcess, ref(dq), 10);
+//
+//    shellThread.join();
+//    monitorThread.detach();
+//
+//    return 0;
+//}
 
-using namespace std;
 
-class Process {
-public:
-    int id;
-    bool isForeground;
-    int waitTime = 0; // Added wait time for each process for simulation
-
-    Process(int id, bool isForeground) : id(id), isForeground(isForeground) {}
-};
-
-class StackNode {
-public:
-    list<shared_ptr<Process>> processes;
-    shared_ptr<StackNode> next;
-    shared_ptr<StackNode> prev;
-
-    StackNode() : next(nullptr), prev(nullptr) {}
-};
-
-class DynamicQueue {
-private:
-    mutex mtx;
-    shared_ptr<StackNode> head;
-    shared_ptr<StackNode> tail;
-    map<int, int> waitQueue; // Map to simulate Wait Queue
-    atomic<int> processCount{ 0 };
-
-public:
-    DynamicQueue() {
-        head = make_shared<StackNode>();
-        tail = head;
-    }
-
-    void enqueue(shared_ptr<Process> process) {
-        lock_guard<mutex> lock(mtx);
-        shared_ptr<StackNode> newNode = make_shared<StackNode>();
-        newNode->processes.push_back(process);
-        if (process->isForeground) {
-            if (head != tail || head->processes.empty()) {
-                newNode->next = head;
-                head->prev = newNode;
-                head = newNode;
-            }
-            else {
-                head->processes.push_back(process);
-            }
-        }
-        else {
-            if (tail->processes.empty()) {
-                tail->processes.push_back(process);
-            }
-            else {
-                newNode->prev = tail;
-                tail->next = newNode;
-                tail = newNode;
-            }
-        }
-        processCount++;
-        waitQueue[process->id] = process->waitTime; // Simulate initial wait time
-    }
-
-    void printQueue() {
-        lock_guard<mutex> lock(mtx);
-        cout << "Running: [" << processCount.load() << "]\n";
-        cout << "---------------------------\n";
-        cout << "DQ: ";
-
-        auto printProcesses = [](const list<shared_ptr<Process>>& processes) {
-            for (auto& p : processes) {
-                cout << p->id << (p->isForeground ? "F " : "B ") << " ";
-            }
-            };
-
-        if (tail) {
-            for (auto current = tail; current != nullptr; current = current->prev) {
-                if (current->prev) {
-                    printProcesses(current->processes);
-                    cout << "(bottom)\n";
-                    cout << "P => ";
-                }
-                else {
-                    printProcesses(current->processes);
-                    cout << "(top)\n";
-                }
-            }
-        }
-
-        cout << "---------------------------\n";
-        cout << "WQ: ";
-        for (auto& wp : waitQueue) {
-            cout << "[" << wp.first << ":" << wp.second << "] ";
-        }
-        cout << "\n...\n";
-    }
-};
-
-int main() {
-    auto dq = make_shared<DynamicQueue>();
-    dq->enqueue(make_shared<Process>(0, true));  // Foreground
-    dq->enqueue(make_shared<Process>(1, false)); // Background
-    this_thread::sleep_for(chrono::seconds(1));
-    dq->printQueue();
-
-    dq->enqueue(make_shared<Process>(2, false));
-    dq->enqueue(make_shared<Process>(3, true));
-    dq->enqueue(make_shared<Process>(4, false));
-    dq->enqueue(make_shared<Process>(5, true));
-    this_thread::sleep_for(chrono::seconds(1));
-    dq->printQueue();
-
-    return 0;
-}
 
 
 
